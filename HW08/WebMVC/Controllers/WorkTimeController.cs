394abee13a1a05dd1d37456882378925062651fc
BalashovM <mi-h-a@mail.ru>
2021-12-12 16:57:58 +0700

    Homework 08 - WO DB

---
+++ HW08/WebMVC/Controllers/WorkTimeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class WorkTimeController : Controller
+    {
+        private readonly ILogger<WorkTimeController> _logger;
+
+        public WorkTimeController(ILogger<WorkTimeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new WorkTimeViewModel());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HW08/WebMVC/Models/WorkTimeViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WebMVC.Models
+{
+    public class WorkTimeViewModel
+    {
+        public string StartWorkTime { get; set; }
+        public string EndWorkTime { get; set; }
+        public string StartLunchTime { get; set; }
+        public string EndLunchTime { get; set; }
+
+        public WorkTimeViewModel()
+        {
+            StartWorkTime = "8:00";
+            EndWorkTime = "20:00";
+            StartLunchTime = "13:00";
+            EndLunchTime = "14:00";
+        }
+    }
+}
